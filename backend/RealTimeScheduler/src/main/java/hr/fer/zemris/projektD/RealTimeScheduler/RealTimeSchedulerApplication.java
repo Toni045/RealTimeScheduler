01c6e49726e21700a12738e4b39a8c53a852b049
Toni045 <t.serezlija@gmail.com>
2024-11-14 23:07:32 +0100

    initialized spring boot project

---
+++ backend/RealTimeScheduler/src/main/java/hr/fer/zemris/projektD/RealTimeScheduler/RealTimeSchedulerApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.zemris.projektD.RealTimeScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeSchedulerApplication.class, args);
+	}
+
+}
---
+++ backend/RealTimeScheduler/src/test/java/hr/fer/zemris/projektD/RealTimeScheduler/RealTimeSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.zemris.projektD.RealTimeScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
